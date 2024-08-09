b21ae78e7d1418a11fd95f0f7f1acf366b77b48b
saikumar <saikumarlaveti5061@gmail.com>
2024-08-09 22:49:46 +0530

    Day_4

--- src/Day_4.java
+++ src/Day_4.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 public class Day_4 {
+    public static void fromScanner()
+    {
+        Scanner sc = new Scanner(System.in);
+        int array[][] = new int [2][3];
+        System.out.println("Enter elements");
+        for(int i =0;i<array.length;i++){
+            for(int j =0;j<=array.length;j++){
+                array[i][j] = sc.nextInt();
+            }
+        }
+        for(int i =0;i< array.length;i++){
+            for(int j = 0;j<=array.length;j++){
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6};
         int brr[][] = {{1,2,3},{4,5,6}};
@@ -8,5 +28,6 @@ public class Day_4 {
             }
             System.out.println();
         }
+        Day_4.fromScanner();
     }
 }
