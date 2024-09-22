27ceba7f98ef1c1725d528ada907ba04592726e6
saikumar <saikumarlaveti5061@gmail.com>
2024-09-22 11:22:29 +0530

    Circular LinkedList completed

--- src/LinkedList/CircularLL/CircularLinkedList.java
+++ src/LinkedList/CircularLL/CircularLinkedList.java
@@ -9,14 +9,20 @@ public class CircularLinkedList {
     }
     public  void display()
     {
-        CircularNode temp = head;
-        while(temp != null)
+        if(head == null)
         {
-            System.out.print(temp.data+" ");
-            temp = temp.next;
+            System.out.println("Circular List is Empty");
+            return;
         }
+        CircularNode temp = head;
+        do{
+            System.out.print(temp.data +" ");
+            temp = temp.next;
+        } while(temp != head);
+
         System.out.println();
     }
+
     public  void  insertAtFirst(int data)
     {
         CircularNode newNode   = new CircularNode(data);
@@ -24,13 +30,14 @@ public class CircularLinkedList {
         {
             head  = newNode;
             tail = newNode;
+            tail.next = head;
         }
         else
         {
             newNode.next = head;
             head = newNode;
+            tail.next = head;
         }
-        // tail.next = head;
 
     }
 
@@ -44,15 +51,19 @@ public class CircularLinkedList {
         else {
             int count = 1;
             CircularNode temp = head;
-            CircularNode prevs = null;
-            while (count < pos && temp.next != null) {
-                prevs = temp;
+            CircularNode prev = null;
+            while (count < pos && temp.next != head) {
+                prev = temp;
                 temp = temp.next;
                 count++;
             }
+            prev.next = newNode;
             newNode.next = temp.next;
-            prevs.next = newNode;
 
+            if(temp == head)
+            {
+                tail = newNode;
+            }
         }
     }
 
@@ -61,6 +72,7 @@ public class CircularLinkedList {
         if(head == null)
         {
             head = tail = newNode;
+            newNode.next = head;
         }
         else
         {
@@ -68,6 +80,50 @@ public class CircularLinkedList {
             tail.next = newNode;
             tail = newNode;
         }
+    }
+    public void deleteAtFirst()
+    {
+        if(head == null)
+        {
+            System.out.println("Circular Linked List is empty");
+        }
+        else {
+            CircularNode temp = head;
+            temp = temp.next;
+            head = temp;
+            tail.next = temp;
+        }
+    }
+
+    public void deleteAtMiddle(int pos)
+    {
+        if(pos == 1) {
+            deleteAtFirst();
+            return;
+        }
+        CircularNode temp = head;
+        CircularNode prev = null;
+        int count = 1;
+        while(count<pos && temp.next != head){
+            prev = temp;
+            temp = temp.next;
+            count++;
+        }
+        prev.next = temp.next;
+        temp.next = null;
+    }
+
+    public void deleteAtLast()
+    {
+        CircularNode temp = head;
+        CircularNode prev = null;
+        while(temp != tail)
+        {
+            prev = temp;
+            temp = temp.next;
+        }
+        tail = prev;
+        tail.next = head;
     }
 
     public static void main(String[] args)
@@ -80,7 +136,17 @@ public class CircularLinkedList {
         cll.insertAtLast(5);
         cll.display();
 
-        cll.insertAtMiddle(5,20);
+        cll.insertAtMiddle(2,20);
         cll.display();
+
+        cll.deleteAtFirst();
+        cll.display();
+
+        cll.deleteAtMiddle(2);
+        cll.display();
+
+        cll.deleteAtLast();
+        cll.display();
+
     }
 }
