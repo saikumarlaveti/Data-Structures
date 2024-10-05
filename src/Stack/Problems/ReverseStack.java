39ee8abcd8fce0cc88271512e67988931b13e83b
saikumar <saikumarlaveti5061@gmail.com>
2024-10-05 19:51:10 +0530

    StockSpan

--- src/Stack/Problems/ReverseStack.java
+++ src/Stack/Problems/ReverseStack.java
@@ -3,9 +3,32 @@ package Stack.Problems;
 import java.util.Stack;
 
 public class ReverseStack {
+    public void pushButtomOfStack(Stack<Integer> s,int data)
+    {
+        if(s.isEmpty())
+        {
+            s.push(data);
+        }
+        else {
+            int element = s.pop();
+            pushButtomOfStack(s,data);
+            s.push(element);
+        }
+
+    }
     public void reverseNumber(Stack<Integer> s)
     {
-         
+
+         if(s.isEmpty())
+         {
+    return;
+         }
+         else
+         {
+            int element = s.pop();
+            reverseNumber(s);
+            pushButtomOfStack(s,element);
+         }
     }
 
     public static void main(String[] args) {
@@ -15,6 +38,11 @@ public class ReverseStack {
         s.push(2);
         s.push(3);
         rs.reverseNumber(s);
+        while( !s.isEmpty())
+        {
+            System.out.print(s.pop()+" ");
+
+        }
 
     }
 }
