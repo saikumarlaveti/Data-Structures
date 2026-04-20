393fc1fcf764804a9734618c75aa0e43d548ce62
saikumar <saikumarlaveti5061@gmail.com>
2026-04-20 23:31:29 +0530

    stack implementation

--- src/Solution.java
+++ src/Solution.java
@@ -1,73 +1,95 @@
-//class Solution{
-//    public String removeSpaces(String name){
-//        //StringBuffer word  = new StringBuffer();
-//        String word = "";
-//        for(int i = 0;i<name.length();i++){
-//            if(name.charAt(i) == ' ' || name.charAt(i) == ',' || name.charAt(i) == ':'){
-//                continue;
-//            }
-//            else {
-//                //word.append(name.charAt(i));
-//                word += name.charAt(i);
-//            }
-//        }
-//        return word;
-//    }
-//    public boolean pal(String name){
-//        name = name.toLowerCase();
-//        String word = removeSpaces(name);
-//        String original = word;
-//        String reverese = "";
-//        for(int i = word.length()-1;i>=0;i--){
-//                    reverese += word.charAt(i);
-//                }
-//        System.out.println(word);
-//        System.out.println(reverese);
-//        return original.equals(reverese);
-//    }
-//    public static void main(String[] args) {
-//      //  String name = "A man, a plan, a canal: Panama";
-//        String name = "Saiku   ma,r";
-//        Solution s = new Solution();
-//        System.out.println(s.pal(name));
-//
-//    }
-//}
-class Solution {
-    public boolean isPalindrome(String s) {
-         s = s.toLowerCase();
-        String remove = "";
-        for(int i = 0;i<s.length();i++){
-            if(s.matches("[a-zA-Z]")){
-                remove += s.charAt(i);
-            }
-            else {
-                continue;
-            }
-//            if(s.charAt(i) <='z' || s.charAt(i) >='a'){
-//                remove += s.charAt(i);
-//            }
-//            else{
-//                continue;
-//            }
-        }
-        String reverse = "";
+import java.util.Scanner;
 
-        for(int i = remove.length()-1;i>=0;i--){
-            reverse += remove.charAt(i);
+class Solution{
+    static int[] arr;
+    static int index =-1;
+    public static void stackImpl(int[] arr){
+        /*
+        1.push( )
+        2.pop ( )
+        3.peek( )
+        4.isEmpty( )
+        */
+          }
+
+          //push ()
+    public static void stackPush(int element) {
+        if(arr.length-1 == index){
+            System.out.println("Stack is Full ");
         }
-        System.out.println(remove);
-        System.out.println(reverse);
-        return remove.equals(reverse);
+        arr[++index] = element;
+
     }
-        public static void main(String[] args) {
-            // String name = "A man, a plan, a canal: Panama";
-        //String name = "Saiku   ma,r";
-        Solution s = new Solution();
-        String name = "race a car";
-        System.out.println(s.isPalindrome(name));
 
+    //pop()
+    public  static void stackPop(){
+        if(index ==-1){
+            System.out.println("Stack is Empty");
+            return;
+        }
+        System.out.println("Poped element is " + arr[index]);
+        index--;
+    }
 
+    //peek()
+    public static int statckpeek(){
+        if(index == -1){
+            return -1;
+        }
+        System.out.println("peek element is " + arr[index]);
+        return arr[index];
+    }
 
+    //isEmpth()
+    public static boolean stackIsEmpty(){
+        return index == -1;
+    }
+
+    //print Stack
+    public static void printStack(){
+        for(int i = 0;i<=index;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the stack :: ");
+        int length = sc.nextInt();
+        arr = new int[length];
+        System.out.println("How many operations : ");
+        int operations = sc.nextInt();
+        for(int i = 0;i<operations;i++) {
+            System.out.println("\nChoose your option :  \n 1.push() \n 2.pop() \n 3.peek() \n 4.isEmpty() \n 5.print()");
+            int option = sc.nextInt();
+            switch (option) {
+                //push ()
+                case 1: {
+                    System.out.print("Enter element ");
+                    int element = sc.nextInt();
+                    stackPush(element);
+                    break;
+                }
+                //pop ()
+                case 2: {
+                    stackPop();
+                    break;
+                }
+                case 3: {
+                    System.out.println(statckpeek());
+                    break;
+                }
+                case 4: {
+                    System.out.println(stackIsEmpty());
+                    break;
+                }
+                case 5: {
+                    printStack();
+                    break;
+                }
+                default:{
+                    System.out.println("Invalid Choose");
+                }
+            }
+        }
     }
 }
