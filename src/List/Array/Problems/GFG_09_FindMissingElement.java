1ce9cedf9bec12a91d0282cf4d03d3d6f4e0f105
saikumar <saikumarlaveti5061@gmail.com>
2026-04-28 21:00:15 +0530

    Finding mIssing element _28_04

--- src/List/Array/Problems/GFG_09_FindMissingElement.java
+++ src/List/Array/Problems/GFG_09_FindMissingElement.java
@@ -1,23 +1,22 @@
 package List.Array.Problems;
 
 public class GFG_09_FindMissingElement {
-    public static int findMissingElement(int[] arr){
-        int Element = -1;
-        boolean flag = false;
-        for(int i = 0;i<arr.length;i++){
-            for(int j = 0;j<=arr.length;j++){
-                if(arr[i] == j){
-                    flag = true;
-                }
-            }
-            if(!flag){
-                Element = arr[i];
-            }
+
+    public static int findMissingElement(int[] arr) {
+        int n = arr.length + 1; // because one number is missing
+
+        int expectedSum = n * (n + 1) / 2;
+
+        int actualSum = 0;
+        for (int num : arr) {
+            actualSum += num;
         }
-        return Element;
+
+        return expectedSum - actualSum;
     }
+
     public static void main(String[] args) {
-        int[] arr = {1,2,3,5};
-        System.out.println(findMissingElement(arr));
+        int[] arr = {1, 3, 4};
+        System.out.println(findMissingElement(arr)); // Output: 2
     }
 }
