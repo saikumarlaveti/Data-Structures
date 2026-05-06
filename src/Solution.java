ed7b22deb6b35275b8ce0d776c30ac785abfc39e
saikumar <saikumarlaveti5061@gmail.com>
2026-05-06 22:17:55 +0530

    Single Linked List - insertElementAtFirst and insertElementLast

--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,3 @@
-import java.util.Stack;
-
 class Solution{
 
-    public static boolean validparentheses(String s){
-        Stack<Character> stack = new Stack<>();
-        if(s.isEmpty()){
-            return false;
-        }
-        else{
-            char[] ch = s.toCharArray();
-            for(int  i = 0;i<ch.length;i++){
-                char c = ch[i];
-                if(stack.isEmpty()){
-                    stack.push(c);
-                }
-                else{
-                    if(c=='}' || c==']' || c==')' ){
-                        
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
-    public static void main(String[] args) {
-        String s = "{[()]}";
-    }
 }
