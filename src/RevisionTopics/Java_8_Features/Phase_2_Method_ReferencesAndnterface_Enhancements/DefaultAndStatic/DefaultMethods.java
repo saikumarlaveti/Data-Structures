7e12a2090382da7fdf8fb1d6ca8efe1568c9b49b
saikumar <saikumarlaveti5061@gmail.com>
2026-05-30 23:33:04 +0530

    forEach method in stream api

--- src/RevisionTopics/Java_8_Features/Phase_2_Method_ReferencesAndnterface_Enhancements/DefaultAndStatic/DefaultMethods.java
+++ src/RevisionTopics/Java_8_Features/Phase_2_Method_ReferencesAndnterface_Enhancements/DefaultAndStatic/DefaultMethods.java
@@ -1,5 +1,10 @@
 package RevisionTopics.Java_8_Features.Phase_2_Method_ReferencesAndnterface_Enhancements.DefaultAndStatic;
 
+import List.LinkedList.DoubleLL.DoubleLinkedList;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
 public interface DefaultMethods {
     default void print() {
         System.out.println("Default Methods..");
@@ -12,10 +17,11 @@ class car implements DefaultMethods{
     public void show(String name) {
         System.out.println("My name is : " + name);
     }
+
     public static void main(String[] args) {
     car c= new car();
     c.show("Saikumar");
+
     }
 }
-
 
---
+++ src/RevisionTopics/Java_8_Features/Phase_3_StreamAPI/ForEach.java
@@ -0,0 +1,31 @@
+package RevisionTopics.Java_8_Features.Phase_3_StreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ForEach {
+    public static void main(String[] args) {
+        //Example - 1
+        List<String> names = Arrays.asList("Java","Spring Boot","Mysql");
+        System.out.println("without forEach( ) method :: ");
+        for(int i = 0;i<names.size();i++){
+            System.out.println(names.get(i));
+        }
+        System.out.println("with forEach( ) method :: ");
+        names.forEach(System.out::println);
+
+        //Example -2
+        List<Integer> nums = Arrays.asList(1,2,3,4,5);
+        System.out.println("without forEach( ) method :: ");
+        for(int j = 0;j<nums.size();j++){
+            if(nums.get(j)%2==0){
+                System.out.println(nums.get(j));
+            }
+        }
+        System.out.println("with forEach( ) method :: ");
+        nums.stream().filter(n->n%2==0).forEach(System.out::println) ;
+
+        int[] arr = {5,6,7,8,9,10};
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
