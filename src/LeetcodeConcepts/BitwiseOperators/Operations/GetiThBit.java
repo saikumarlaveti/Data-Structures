7e8aaa9361f63bd0f8c9880e5a2b2b2372e41eea
saikumar <saikumarlaveti5061@gmail.com>
2026-06-03 23:07:40 +0530

    get ith bit

--- src/LeetcodeConcepts/BitwiseOperators/Operations/GetiThBit.java
+++ src/LeetcodeConcepts/BitwiseOperators/Operations/GetiThBit.java
@@ -1,9 +1,11 @@
-package LeetcodeConcepts.BitwiseOperators;
+package LeetcodeConcepts.BitwiseOperators.Operations;
 
 public class GetiThBit {
     public int getithBit(int num,int i){
         int bitMask = 1<<i;
+        System.out.println(num&bitMask);
         if((num&bitMask)==0)
+
             return 0;
         else
             return 1;
@@ -11,8 +13,8 @@ public class GetiThBit {
 
     public static void main(String[] args) {
             GetiThBit gb = new GetiThBit();
-        System.out.println(gb.getithBit(10,3));
-        System.out.println(gb.getithBit(5,1));
-        System.out.println(gb.getithBit(10,1));
+        System.out.println(gb.getithBit(10,3));  // 1
+        System.out.println(gb.getithBit(5,1));    // 0
+        System.out.println(gb.getithBit(10,1));  // 1
     }
 }
