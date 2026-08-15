9e2ba122ef4ebcdde2729668000fa06d49d84e8b
saikumar <saikumarlaveti5061@gmail.com>
2026-08-15 23:29:49 +0530

    Dayd_04_isSubsequence

--- src/LeetcodeConcepts/TwoPointers/isSubsequence_18.java
+++ src/LeetcodeConcepts/TwoPointers/isSubsequence_18.java
@@ -2,25 +2,24 @@ package LeetcodeConcepts.TwoPointers;
 
 public class isSubsequence_18 {
     public static boolean isSubsequence(String s, String t) {
-        if(t.length()==1){
-            return s.charAt(0) == t.charAt(0);
-        }
-        int left = 0;
-    int right = 1;
-    while(right<s.length()){
-        if(!(t.indexOf(s.charAt(left))<=t.indexOf(s.charAt(right)))){
-            return false;
-        }
-        else {
-            left++;
+
+        int left = 0;  // pointer for s
+        int right = 0; // pointer for t
+
+        while (left < s.length() && right < t.length()) {
+
+            if (s.charAt(left) == t.charAt(right)) {
+                left++;
+            }
+
             right++;
         }
-    }
-    return true;
+
+        return left == s.length();
     }
     public static void main(String[] args) {
-        String s = "aza";
-        String t = "abzba";
+        String s = "abc";
+        String t = "ahbgdc";
         System.out.println(isSubsequence(s,t));
     }
 }
