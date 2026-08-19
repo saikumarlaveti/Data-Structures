0d7b0f6d4d655f34d674ef4c533868b88357a13e
saikumar <saikumarlaveti5061@gmail.com>
2026-08-19 23:33:49 +0530

    3 sum problem

--- src/Leetcode/LC_15_3Sum.java
+++ src/Leetcode/LC_15_3Sum.java
@@ -9,22 +9,38 @@ public class LC_15_3Sum {
     public static List<List<Integer>> threeSum(int[] nums) {
         HashSet<List<Integer>> triple = new HashSet<>();
         Arrays.sort(nums);
-        int i = 0;
-        int j = i+1;
-        while(j<=nums.length) {
-            int k = j+1;
-            while (k < nums.length) {
-                if (nums[i] + nums[j] + nums[k] == 0) {
-                    triple.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                    i++;j++;
-                    k = j+1;
-                }
-                else{
-                    k++;
+        for(int i = 0;i<nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                for(int k = j+1;k<nums.length;k++){
+                    if(nums[i] + nums[j]+nums[k] ==0){
+                        triple.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    }
                 }
             }
-            i++;
-            j++;
+        }
+        return new ArrayList<>(triple);
+    }
+
+    //method - 2
+
+    public static List<List<Integer>> threeSum_02(int[] nums) {
+        HashSet<List<Integer>> triple = new HashSet<>();
+        Arrays.sort(nums);
+        for(int i = 0;i<nums.length-2;i++){
+         int left = i+1;
+         int right = nums.length-1;
+         while(left<right){
+             int sum = nums[i]+nums[left]+nums[right] ;
+             if(sum == 0){
+                 triple.add(Arrays.asList(nums[i],nums[left],nums[right]));
+             }
+             else  if(sum<0){
+                 left++;
+             }
+             else{
+                 right--;
+             }
+         }
         }
         return new ArrayList<>(triple);
     }
