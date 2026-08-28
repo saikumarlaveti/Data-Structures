05dbe64798d1180791724c5c92cba77e1c8c1491
saikumar <saikumarlaveti5061@gmail.com>
2026-08-28 23:11:41 +0530

    prefix sum

--- src/Leetcode/LC_1732_largestAltitude.java
+++ src/Leetcode/LC_1732_largestAltitude.java
@@ -14,8 +14,17 @@ public class LC_1732_largestAltitude {
 
         return maxAltitude;
     }
+    public static int largestAltitude_01(int[] gain) {
+        int[] result = new int[gain.length+1];
+        int largestAltitude = 0;
+        for(int i = 1;i<result.length;i++){
+            result[i] = gain[i-1]+result[i-1];
+            largestAltitude = Math.max(result[i],largestAltitude);
+        }
+        return largestAltitude;
+    }
     public static void main(String[] args) {
-        int[] arr = {44,32,-9,52,23,-50,50,33,-84,47,-14,84,36,-62,37,81,-36,-85,-39,67,-63,64,-47,95,91,-40,65,67,92,-28,97,100,81};
-        System.out.println(largestAltitude(arr));
+      int[] arr = {44,32,-9,52,23,-50,50,33,-84,47,-14,84,36,-62,37,81,-36,-85,-39,67,-63,64,-47,95,91,-40,65,67,92,-28,97,100,81};
+        System.out.println(largestAltitude_01(arr));
     }
 }
