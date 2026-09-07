e802ef22d0c9de712bb4ec753609db708901319b
saikumar <saikumarlaveti5061@gmail.com>
2026-09-07 23:10:01 +0530

    AntonTheBoundary

--- src/Empty.java
+++ src/Empty.java
@@ -1,10 +1,14 @@
+@FunctionalInterface
 interface Empty{
-    public void addition(int a, int b);
+     void addition(int a, int b);
+     //void subtraction(int a,int b);
 }
 
 class EmptyImpl{
     public static void main(String[] args) {
         Empty e = (a,b)->System.out.println(a+b);
+        Empty e1 = (a,b) -> System.out.println(a-b);
         e.addition(2,3);
+        e1.addition(2,3);
     }
 }
---
+++ src/Leetcode/LC_3028_AntOnTheBoundary.java
@@ -0,0 +1,15 @@
+package Leetcode;
+
+public class LC_3028_AntOnTheBoundary {
+    public int returnToBoundaryCount(int[] nums) {
+        int sum = 0;
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            sum += nums[i];
+            if(sum == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
